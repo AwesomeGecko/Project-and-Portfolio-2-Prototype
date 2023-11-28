46fa61c0b4410d8ea18ed590fde97655760f7519
AnthonyTartagliaFSU <AGTartaglia@student.fullsail.edu>
2023-11-28 12:21:55 -0500

    Added sprinting and crouching to player
    Player can now sprint with left shift and crouch with left ctrl

--- PaP2 Prototype/Assets/Scripts/PlayerController.cs
+++ PaP2 Prototype/Assets/Scripts/PlayerController.cs
@@ -9,22 +9,29 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float playerSpeed;
     [SerializeField] float jumpHeight;
     [SerializeField] float gravityValue;
+    [SerializeField] float sprintMod;
+    [SerializeField] float crouchMod;
+    [SerializeField] float crouchDist;
+    //[SerializeField] float crouchTransitionSpeed;
 
     private Vector3 playerVelocity;
     private Vector3 move;
     private bool groundedPlayer;
     private int jumpCount;
+    private Vector3 crouchCameraDist;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        crouchCameraDist = new Vector3(0, crouchDist / 2, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Sprint();
+        Crouch();
         //Identical movement code in the lectures
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
@@ -37,7 +44,7 @@ public class PlayerController : MonoBehaviour
 
         controller.Move(move * playerSpeed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && jumpCount < 1)
+        if (Input.GetButtonDown("Jump") && jumpCount < 1)
         {
             playerVelocity.y = jumpHeight;
             jumpCount++;
@@ -45,5 +52,35 @@ public class PlayerController : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void Sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            playerSpeed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            playerSpeed /= sprintMod;
+        }
+    }
+
+    void Crouch()
+    {
+        if (Input.GetButtonDown("Crouch"))
+        {
+            controller.height -= crouchDist;
+            playerSpeed *= crouchMod;
+            Camera.main.transform.localPosition -= crouchCameraDist;
+            //Debug.Log("player is crouching");
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            controller.height += crouchDist;
+            playerSpeed /= crouchMod;
+            Camera.main.transform.localPosition += crouchCameraDist;
+            //Debug.Log("player is no longer crouching");
+        }
     }
 }
