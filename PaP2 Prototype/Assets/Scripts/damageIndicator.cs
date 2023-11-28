6de6e5a144f7c84ccae336addfba4877aa23a581
SpaceCoconutz <zayne@promonolo.org>
2023-11-28 15:06:05 -0600

    Updated Prefabs and Profiles
    - Updated UI prefab to include the new features (pick up, ammo counter)
    
    - moved the Post Processing Profile to UI parts folder for better organization
    
    - Made changes to the damage indicator script, added comments for user friendly reading

--- PaP2 Prototype/Assets/Scripts/damageIndicator.cs
+++ PaP2 Prototype/Assets/Scripts/damageIndicator.cs
@@ -3,34 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
-public class takeDamageScript : MonoBehaviour
+public class damageIndicator : MonoBehaviour
 {
     //This is only a start and will update when we go over it in the lecture
+    //Below is explinations of what is going on
 
-    public float intensity = 0;
+    private float intensity;
 
     PostProcessVolume volume;
     Vignette vignette;
 
-    // Start is called before the first frame update
     void Start()
     {
+        //Finds the PostProcessVolume
         volume = GetComponent<PostProcessVolume>();
+        //Finds the Vignette
         volume.profile.TryGetSettings<Vignette>(out vignette);
 
         if (!vignette)
         {
+            //if the Vignette is not avalible or emty
             Debug.Log("error, empty vignette");
         }
         else
         {
+            //turns off the Vignette by default
             vignette.enabled.Override(false);
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //ONLY FOR TESTING PERPOUSES
+        //Uses the left mouse button to show the effect
         if (Input.GetMouseButtonDown(0))
         {
             StartCoroutine(TakeDamageEffect());
@@ -40,20 +45,30 @@ public class takeDamageScript : MonoBehaviour
     IEnumerator TakeDamageEffect()
     {
         intensity = 0.4f;
+
+        //Turns on Vignette
         vignette.enabled.Override(true);
+        //Sets intensity
         vignette.intensity.Override(0.4f);
         yield return new WaitForSeconds(0.4f);
 
+
+        //waites for the intensity to go back to 0
         while (intensity > 0)
         {
             intensity -= 0.01f;
+
+            //once the intensity goes below 0
             if (intensity < 0)
-            { 
+            {
                 intensity = 0;
             }
+            //Vignette intensity is updated
             vignette.intensity.Override(intensity);
             yield return new WaitForSeconds(0.01f);
         }
+
+        //once the intinsity is at 0 it turns off the Vignette
         vignette.enabled.Override(false);
         yield break;
     }
