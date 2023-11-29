a66216ee9321dbba6e7f4b1fb27dc4f41dadad27
Casey-Muxlow <CWMuxlow@student.fullsail.edu>
2023-11-29 17:52:51 -0500

    Bullet prefab and IDamage
    Bullet damage prefab and the IDamage scripts are complete.

---
+++ PaP2 Prototype/Assets/Scripts/bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+
+    [SerializeField] int damage;
+    [SerializeField] int destroyTime;
+    [SerializeField] int speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, destroyTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+            return;
+
+        IDamage dmg = other.GetComponent<IDamage>();
+
+        if (dmg != null)
+        {
+            dmg.takeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
