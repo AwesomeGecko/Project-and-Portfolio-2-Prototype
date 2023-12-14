a06f9a55c20570cf1cbcd7c3cf065c66a3cf0b56
SpaceCoconutz <zayne@promonolo.org>
2023-12-13 23:30:58 -0600

    Audio Changes

--- PaP2 Prototype/Assets/Scripts/AudioControls.cs
+++ PaP2 Prototype/Assets/Scripts/AudioControls.cs
@@ -1,22 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioControls : MonoBehaviour
 {
-    [SerializeField] AudioSource music;
-    [SerializeField] AudioSource soundEffects;
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Slider mainSlider;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
 
     private void Start()
     {
-        music = gameManager.instance.cameraObject.GetComponent<AudioSource>();
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else 
+        {
+            setMusicVolume();
+            setSFXVolume();
+            setMainVolume();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setMusicVolume()
     {
-        
+        float volume = musicSlider.value;
+        audioMixer.SetFloat("Music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
+    public void setSFXVolume()
+    {
+        float volume = sfxSlider.value;
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+    }
+
+    public void setMainVolume()
+    {
+        float volume = mainSlider.value;
+        audioMixer.SetFloat("Main", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("mainVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        mainSlider.value = PlayerPrefs.GetFloat("mainVolume");
+
+        setMusicVolume();
+        setSFXVolume();
+        setMainVolume();
+    }
     
 }
