c006763168957f4b020d61e19c822d2495c057c9
Samuel Range <SDRange@student.fullsail.edu>
2023-12-13 18:52:49 -0600

    enemy change plus new enemy asset

--- PaP2 Prototype/Assets/Scripts/EnemyAI.cs
+++ PaP2 Prototype/Assets/Scripts/EnemyAI.cs
@@ -14,7 +14,6 @@ public class EnemyAI : MonoBehaviour, IDamage
     [SerializeField] Transform headPos;
     [SerializeField] Animator anim;
     [SerializeField] Renderer model;
-
     [SerializeField] AudioSource aud;
     [SerializeField] Collider damageCol;
 
