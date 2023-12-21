0eba3ae5b252d4c74bab3b0d97094f4553615676
Casey-Muxlow <CWMuxlow@student.fullsail.edu>
2023-12-21 00:17:45 -0500

    Boss Upgrades
    Boss now shoots from all cannons.

--- PaP2 Prototype/Assets/Scripts/Boss Script.cs
+++ PaP2 Prototype/Assets/Scripts/Boss Script.cs
@@ -28,13 +28,22 @@ public class BossScript : MonoBehaviour, IDamage
 
     [Header("----- Weapon -----")]
     [SerializeField] GameObject EnemyBullet;
-    [SerializeField] int bulletDamage;
-    [SerializeField] int bulletDestroyTime;
-    [SerializeField] int bulletSpeed;
+    [SerializeField] int CanonbulletDamage;
+    [SerializeField] int CanonbulletDestroyTime;
+    [SerializeField] int CanonbulletSpeed;
+    [SerializeField] int SmallcanonbulletDamage;
+    [SerializeField] int SmallcanonbulletDestroyTime;
+    [SerializeField] int SmallcanonbulletSpeed;
     [SerializeField] float shootRate;
     [SerializeField] float shootSpeed;
     [SerializeField] Transform shootPos;
     [SerializeField] Transform shootPos2;
+    [SerializeField] Transform shootPos3;
+    [SerializeField] Transform shootPos4;
+    [SerializeField] Transform shootPos5;
+    [SerializeField] Transform shootPos6;
+    [SerializeField] Transform shootPos7;
+    [SerializeField] Transform shootPos8;
 
     [Header("----- Audio -----")]
     [SerializeField] AudioClip hitSound;
@@ -188,6 +197,7 @@ public class BossScript : MonoBehaviour, IDamage
     {
         playerDir = gameManager.instance.player.transform.position - headPos.position;
         angleToPlayer = Vector3.Angle(playerDir, transform.forward);
+        playerDir.Normalize();
 
         RaycastHit hit;
         if (Physics.Raycast(headPos.position, playerDir, out hit))
@@ -232,14 +242,47 @@ public class BossScript : MonoBehaviour, IDamage
 
     public void CreateBullet()
     {
+        //In the animation controller use the shoot animation of BigCanon_A
         GameObject newBullet1 = Instantiate(EnemyBullet, shootPos.position, transform.rotation);
         EnemyBullet enemyBullet1 = newBullet1.GetComponent<EnemyBullet>();
-        enemyBullet1.SetBulletProperties(bulletDamage, bulletDestroyTime, bulletSpeed);    
-        
+        enemyBullet1.SetBulletProperties(CanonbulletDamage, CanonbulletDestroyTime, CanonbulletSpeed);
+
         GameObject newBullet2 = Instantiate(EnemyBullet, shootPos2.position, transform.rotation);
         EnemyBullet enemyBullet2 = newBullet2.GetComponent<EnemyBullet>();
-        enemyBullet2.SetBulletProperties(bulletDamage, bulletDestroyTime, bulletSpeed);
-        
+        enemyBullet2.SetBulletProperties(CanonbulletDamage, CanonbulletDestroyTime, CanonbulletSpeed);
+    }
+    public void CreateCanon_BBullet()
+    {
+        //In the animation controller use the shoot animation of BigCanon_B
+        GameObject newBullet3 = Instantiate(EnemyBullet, shootPos3.position, transform.rotation);
+        EnemyBullet enemyBullet3 = newBullet3.GetComponent<EnemyBullet>();
+        enemyBullet3.SetBulletProperties(CanonbulletDamage, CanonbulletDestroyTime, CanonbulletSpeed);
+
+        GameObject newBullet4 = Instantiate(EnemyBullet, shootPos4.position, transform.rotation);
+        EnemyBullet enemyBullet4 = newBullet4.GetComponent<EnemyBullet>();
+        enemyBullet4.SetBulletProperties(CanonbulletDamage, CanonbulletDestroyTime, CanonbulletSpeed);
+    }
+    public void CreateSmallCanon_ABullet()
+    {
+        //In the animation controller use the shoot animation of SmallCanon_A
+        GameObject newBullet5 = Instantiate(EnemyBullet, shootPos5.position, transform.rotation);
+        EnemyBullet enemyBullet5 = newBullet5.GetComponent<EnemyBullet>();
+        enemyBullet5.SetBulletProperties(SmallcanonbulletDamage, SmallcanonbulletDestroyTime, SmallcanonbulletSpeed);
+
+        GameObject newBullet6 = Instantiate(EnemyBullet, shootPos6.position, transform.rotation);
+        EnemyBullet enemyBullet6 = newBullet6.GetComponent<EnemyBullet>();
+        enemyBullet6.SetBulletProperties(SmallcanonbulletDamage, SmallcanonbulletDestroyTime, SmallcanonbulletSpeed);
+    }
+    public void CreateSmallCanon_BBullet()
+    {
+            //In the animation controller use the shoot animation of SmallCanon_B
+            GameObject newBullet7 = Instantiate(EnemyBullet, shootPos7.position, transform.rotation);
+        EnemyBullet enemyBullet7 = newBullet7.GetComponent<EnemyBullet>();
+        enemyBullet7.SetBulletProperties(SmallcanonbulletDamage, SmallcanonbulletDestroyTime, SmallcanonbulletSpeed);
+
+        GameObject newBullet8 = Instantiate(EnemyBullet, shootPos8.position, transform.rotation);
+        EnemyBullet enemyBullet8 = newBullet8.GetComponent<EnemyBullet>();
+        enemyBullet8.SetBulletProperties(SmallcanonbulletDamage, SmallcanonbulletDestroyTime, SmallcanonbulletSpeed);
     }
 
     public void takeDamage(int amount)
