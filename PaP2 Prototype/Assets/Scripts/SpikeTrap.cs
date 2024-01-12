4f375d8920bedc2d7e400d1e98cc74b980b365c6
creid42091 <110431207+creid42091@users.noreply.github.com>
2024-01-12 15:30:12 -0700

    Spike Trap Update
    Made the spike trap look similar to actual spikes and updated the script for the spikes to constantly move up and down

--- PaP2 Prototype/Assets/Scripts/SpikeTrap.cs
+++ PaP2 Prototype/Assets/Scripts/SpikeTrap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,16 @@ using UnityEngine;
 public class SpikeTrap : MonoBehaviour
 {
     [SerializeField] int damageAmount;
+    [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] float maxHeight = 2.0f;
+    [SerializeField] float minHeight = 0.5f;
+
+    private bool retract = true;
+
+    private void Update()
+    {
+        TrapActive();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,6 +29,17 @@ public class SpikeTrap : MonoBehaviour
                 // Disables the Trap
                 // gameObject.SetActive(false);
             }
+        }
+    }
+
+    private void TrapActive()
+    {
+        float newHeight = retract ? maxHeight : minHeight;
+        float calculateHeight = Mathf.MoveTowards(transform.position.y, newHeight, moveSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, calculateHeight, transform.position.z);
+        if(Mathf.Approximately(calculateHeight, newHeight))
+        {
+            retract = !retract;
         }
     }
 }
