4c6f0168c56a804093a34d0a9b68729d5e9fc221
Christopher Robbins <zayne@promonolo.org>
2024-01-17 20:05:55 -0600

    Save/Load FINALLY works
    - Hot fix to save system
    ....it fixed itself....
    - now i can make player data save and load from anywhere

--- PaP2 Prototype/Assets/Scripts/DataPersistence/FileDataHandler.cs
+++ PaP2 Prototype/Assets/Scripts/DataPersistence/FileDataHandler.cs
@@ -18,7 +18,7 @@ public class FileDataHandler
     public GameData Load()
     {
         //Path.Combine allows any system to use the file path. MAC, Windows, etc. all have their own set up for file paths
-        string fullPath = Path.Combine(Application.persistentDataPath, dataDirPath, dataFileName);
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
         GameData LoadedData = null;
         if (File.Exists(fullPath))
         {
@@ -26,16 +26,17 @@ public class FileDataHandler
             {
                 //load the serialized data from file
                 string dataToLoad = "";
-                using (FileStream stream = new FileStream(fullPath, FileMode.Open, FileAccess.Read))
+                using (FileStream stream = new FileStream(fullPath, FileMode.Open))
                 {
                     using (StreamReader reader = new StreamReader(stream))
-                    { 
+                    {
                         dataToLoad = reader.ReadToEnd();
                     }
                 }
 
                 //deserialize the data from the JSON file back to the object
                 LoadedData = JsonUtility.FromJson<GameData>(dataToLoad);
+                Debug.Log("Loaded Data!");
             }
             catch (Exception e)
             {
@@ -48,25 +49,26 @@ public class FileDataHandler
     public void Save(GameData data)
     {
         //Path.Combine allows any system to use the file path. MAC, Windows, etc. all have their own set up for file paths
-        string fullPath = Path.Combine(Application.persistentDataPath, dataDirPath, dataFileName);
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
         try
         {
-            DirectoryInfo directoryInfo = Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
-            directoryInfo.Attributes &= ~FileAttributes.ReadOnly;
             //creates directory path if it doesnt exist
-            //Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+            Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
 
             //serialize game data into a JSON file
             string dataToStore = JsonUtility.ToJson(data, true);
 
             //writes the data to the file
-            using (FileStream stream = new FileStream(fullPath, FileMode.Create, FileAccess.Write))
+            using (FileStream stream = new FileStream(fullPath, FileMode.Create))
             {
                 using (StreamWriter writer = new StreamWriter(stream))
-                { 
+                {
                     writer.Write(dataToStore);
+
                 }
             }
+
+            Debug.Log("Saved Data!");
         }
         catch (Exception e)
         {
