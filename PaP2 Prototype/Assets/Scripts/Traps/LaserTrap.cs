7d301e4804640eae7787b21d5bf9d0cd22c26b58
creid42091 <110431207+creid42091@users.noreply.github.com>
2024-01-18 12:45:35 -0700

    Level Design and Laser Trap Update
    Added various traps and platforming throughout the 1st section of my building. Also, updated the laser trap to move back and forth between two fixed positions. Giving the player more of a challenge to avoid the lasers.

--- PaP2 Prototype/Assets/Scripts/Traps/LaserTrap.cs
+++ PaP2 Prototype/Assets/Scripts/Traps/LaserTrap.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class LaserTrap : MonoBehaviour
 {
+    // Notes: Set the Start Point to the original position of the laser. Adjust the End Point
+    // x or y values. The laser will now bounce back and forth between two points.
+    // IMPORTANT! Make sure Point to Point flag is set to TRUE(checked).
+
+    [Header("Damage")]
     [SerializeField] int dmgAmount;
     [SerializeField] int burnAmt;
     [SerializeField] int burnOverTime;
+
+    [Header("Position")]
+    [SerializeField] bool pointToPoint;
+    [SerializeField] Vector3 startPoint;
+    [SerializeField] Vector3 endPoint;
+    [SerializeField] float movingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(pointToPoint)
+        {
+            StartCoroutine(Move());
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -36,4 +56,26 @@ public class LaserTrap : MonoBehaviour
             timer += 1f;
         }
     }
+
+    private IEnumerator Move()
+    {
+        do
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPoint, movingSpeed * Time.deltaTime);
+            if (transform.position == endPoint)
+            {
+                yield return new WaitForSeconds(1f);
+                ReverseCourse();
+            }
+            yield return null;
+        }while (true);
+    }
+
+    private void ReverseCourse() // Use a swap method
+    {
+        Vector3 temp = startPoint;
+        startPoint = endPoint;
+        endPoint = temp;
+    }
+    
 }
